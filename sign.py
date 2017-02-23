edf8d7006a903bb9264a6f14e489083006bb1b76
Jason Thomas <jason@lithiumfox.com>
2017-02-23 11:02:11 -0800

    Add support for webextensions

--- sign.py
+++ sign.py
@@ -4,6 +4,7 @@ import json
 import re
 import requests
 import shutil
+import sys
 import tempfile
 import zipfile
 from base64 import b64decode
@@ -46,10 +47,27 @@ def call_signing(file_path, guid, endpoint):
 def get_guid(file_path):
     """Get e-mail guid of add-on."""
     z = zipfile.ZipFile(file_path)
-    with z.open('install.rdf') as install:
-        match = re.search(r'[\w\.-]+@[\w\.-]+', install.read())
-
-    return match.group(0)
+    zlist = z.namelist()
+    if 'install.rdf' in zlist:
+        try:
+            with z.open('install.rdf') as install:
+                match = re.search(r'[\w\.-]+@[\w\.-]+', install.read())
+                return match.group(0)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            exit(1)
+    elif 'manifest.json' in zlist:
+        try:
+            with z.open('manifest.json') as manifest:
+                m = json.loads(manifest.read())
+                return m['applications']['gecko']['id']
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            exit(1)
+    # we didn't find the guid, bail
+    else:
+        print "Could not find guid, check add-on"
+        exit(1)
 
 
 def main():
